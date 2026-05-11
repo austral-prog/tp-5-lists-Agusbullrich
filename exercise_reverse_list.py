5e0ba867a26a206c7b3adb6aabc1acb0a102fd86
Agusbullrich <agusbullrich08@gmail.com>
2026-05-11 11:57:03 -0300

    Add files via upload

---
+++ exercise_reverse_list.py
@@ -0,0 +1,18 @@
+# Ejercicio 8: Invertir una lista
+
+def reverse_list(lista):
+    """
+    Retorna una nueva lista con los elementos en orden inverso.
+
+    Args:
+        lista: Una lista de elementos
+
+    Returns:
+        Una nueva lista con los elementos en orden inverso
+    """
+    if len(lista) > 0:
+        lista1 = lista[-1::-1]
+    else:
+        lista1 = []
+    return lista1
+    pass  # Reemplazar con tu implementación
