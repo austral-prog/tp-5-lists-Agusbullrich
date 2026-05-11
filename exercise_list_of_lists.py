959f928573b2cbf7a4f869915a011bdbb35e4002
Agusbullrich <agusbullrich08@gmail.com>
2026-05-11 10:54:10 -0300

    Add files via upload

--- exercise_list_of_lists.py
+++ exercise_list_of_lists.py
@@ -13,4 +13,10 @@ def list_of_lists(lista_de_listas):
     Returns:
         La lista de listas modificada según las reglas
     """
+
+
+    lista_de_listas[0] = lista_de_listas[0][:2]
+    lista_de_listas[1] = lista_de_listas[1][1:4]
+    lista_de_listas[2] = lista_de_listas[2][-2:]
+    return lista_de_listas
     pass  # Reemplazar con tu implementación
